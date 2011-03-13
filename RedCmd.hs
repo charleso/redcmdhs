d0df3ce25d6cbb5ff6f75aad7412c7fdf6458d2e
Charles O'Farrell <charleso@charleso.org>
2011-03-13 15:30:02 +1000

    Add single HUnit test case

--- RedCmd.hs
+++ RedCmd.hs
@@ -10,6 +10,7 @@ import Text.XML.HXT.Core
 import Text.XML.HXT.TagSoup (withTagSoup)
 
 import Data.Tree
+import Test.HUnit
 
 data Config = Config { 
     username :: String,
@@ -64,3 +65,21 @@ login :: Config -> IO ()
 login config = curlGet (geturl config "login") []
 
 geturl config path = (show $ url config) ++ path ++ "/"
+
+-- Tests
+
+tests = TestList [
+        testParseIssue (Issue {
+            subject = "JVM Custom Property Clean up - EDOI",
+            description = "Part of the JVM Custom Property clean up\n\nedoi.appserver.name - remove and instead derive \n\nedoi.database.product.type - remove and instead derive\n\nedoi.duplicate.key.error.code - remove entirely\n\nedoi.load.used.modules.only.enabled - remove entirely",
+            history = [
+                History {note = "When finished assign back to Matt O to remove from ControlTier"},
+                History {note = "None of these are needed for PD190."}
+            ]
+        }) "test/issue1.html"
+    ]
+
+testParseIssue expected file = TestCase $ do
+        issue <- runX $ parse file >>> parseIssue
+        print issue
+        assertEqual ("Test parsing " ++ file) expected (head issue)
